5eab905290dc705b9a08cd99177015bd2668eeb1
unknown <ulugbekgozixonov@gmail.com>
2025-02-12 18:01:25 +0500

    db changed postgresql

--- database.py
+++ database.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 from config import DATABASE_USER, DATABASE_PORT, DATABASE_HOST, DATABASE_NAME, DATABASE_PASSWORD
 
-DATABASE_SQLITE_URL = "sqlite:///./todos.db"
-engine = create_engine(DATABASE_SQLITE_URL,connect_args={"check_same_thread": False})
+# DATABASE_SQLITE_URL = "sqlite:///./todos.db"
+# engine = create_engine(DATABASE_SQLITE_URL,connect_args={"check_same_thread": False})
 
-# DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
-# engine = create_engine(DATABASE_URL)
+DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
