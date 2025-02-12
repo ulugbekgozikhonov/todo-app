773fe2d7c135980ec769d69c7e9d3951321bf902
unknown <ulugbekgozixonov@gmail.com>
2025-02-12 17:40:58 +0500

    deploy to render com

--- database.py
+++ database.py
@@ -1,12 +1,14 @@
 # pip install sqlalchemy
 from sqlalchemy.engine import create_engine
-from sqlalchemy.orm import sessionmaker,declarative_base
-from config import DATABASE_USER,DATABASE_PORT,DATABASE_HOST,DATABASE_NAME,DATABASE_PASSWORD
-# DATABSE_SQLITE_URL = "sqlite:///./todos.db"
-# engine = create_engine(DATABSE_SQLITE_URL,connect_args={"check_same_thread": False})
+from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABSE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}/{DATABASE_NAME}"
+from config import DATABASE_USER, DATABASE_PORT, DATABASE_HOST, DATABASE_NAME, DATABASE_PASSWORD
 
-engine = create_engine(DATABSE_URL)
-SessionLocal = sessionmaker(bind=engine,autoflush=False,autocommit=False)
+# DATABASE_SQLITE_URL = "sqlite:///./todos.db"
+# engine = create_engine(DATABASE_SQLITE_URL,connect_args={"check_same_thread": False})
+
+DATABASE_URL = f"postgresql://{DATABASE_USER}:{DATABASE_PASSWORD}@{DATABASE_HOST}:{DATABASE_PORT}/{DATABASE_NAME}"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
